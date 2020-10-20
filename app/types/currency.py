7ef4188c1ff219f82e6b27748d0558f1d2f88da2
Victor Caceres <caceres.vc95@gmail.com>
2020-10-19 22:40:01 -0400

    ad gradients type

--- app/types/currency.py
+++ app/types/currency.py
@@ -1,10 +1,10 @@
-from graphene import ObjectType, String, Float, Boolean, InputObjectType, Int
-
+from graphene import ObjectType, String, Float, Boolean, InputObjectType, Int, List
 
 class Currency(ObjectType):
     id = String()
     name = String()
     color = String()
+    gradients = List(String)
     active = Boolean()
     type = String()
     symbol = String()
